2aa8673ff4a7d212e5a41638afa8bc8ad834daf0
ANUMIT JOOLOOR <anumit@gmail.com>
2024-08-17 01:35:31 +0530

    hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@ public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-	System.out.println ("Hello World!");
-    System.out.println("T");
+	    System.out.println ("Hello World!");
+        System.out.println("T");
+    }
+}
+System.out.println("T");
     }
 }
